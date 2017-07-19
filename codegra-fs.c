de44b17fec1b310150e1c0fa935ee77c96383bba
Olmo Kramer <olmo.kramer@gmail.com>
2017-07-19 15:59:22 +0200

    Initial commit

---
+++ codegra-fs.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <fuse.h>
+#include <curl/curl.h>
+
+int main(int argc, const char *argv[argc])
+{
+	(void) argv;
+	return EXIT_SUCCESS;
+}
