76e31f1fc2f8c78e8413b94e1b619044aeb764d3
Olmo Kramer <olmo.kramer@gmail.com>
2017-07-21 10:33:47 +0200

    cgapi: Add login function

--- cgapi.c
+++ cgapi.c
@@ -9,16 +9,20 @@
 #endif
 
 enum req_type {
+	REQ_LOGIN,
 	REQ_ASSIGNMENTS,
 	REQ_SUBMISSIONS,
 	REQ_FILES,
 };
 
 static const char *api_routes[] = {
+	[REQ_LOGIN]       = BASE_URL "/login",
 	[REQ_ASSIGNMENTS] = BASE_URL "/assignments/",
 	[REQ_SUBMISSIONS] = BASE_URL "/assignments/%u/submissions/",
 	[REQ_FILES]       = BASE_URL "/submissions/%u/files/%s",
 };
+
+static char *jwt_token[128];
 
 void cgapi_init(void)
 {
@@ -38,42 +42,54 @@ static CURL *cgapi_init_request(enum req_type type, const char *url)
 	return curl;
 }
 
+int cgapi_login(char *username, char *password)
+{
+	(void) username, (void) password;
+	return 0;
+}
+
 // TODO: Send mkdir request to server
 int cgapi_mkdir(int submission_id, const char *path)
 {
 	(void) submission_id, (void) path;
 	return 0;
 }
+
 // TODO: Send rmdir request to server
 int cgapi_rmdir(int submission_id, const char *path)
 {
 	(void) submission_id, (void) path;
 	return 0;
 }
+
 // TODO: Get assignments
 int cgapi_get_assignments(struct cgapi_assignment *ass)
 {
 	(void) ass;
 	return 0;
 }
+
 // TODO: Get Submissions
 int cgapi_get_submissions(int assignment_id, struct cgapi_submission *subs)
 {
 	(void) assignment_id, (void) subs;
 	return 0;
 }
+
 // TODO: Get submission files
 int cgapi_get_submission_files(int submission_id, struct cgapi_file *files)
 {
 	(void) submission_id, (void) files;
 	return 0;
 }
+
 // TODO: Get other info from server
 int cgapi_get_file_meta(int file_id, struct cgapi_file_meta *fm)
 {
 	(void) file_id, (void) fm;
 	return 0;
 }
+
 // TODO: Get data from server
 int cgapi_get_file_buf(int file_id, struct cgapi_file *f)
 {
@@ -81,12 +97,14 @@ int cgapi_get_file_buf(int file_id, struct cgapi_file *f)
 	// f->buf = ...
 	return 0;
 }
+
 // TODO: Send f->buf to server
 int cgapi_put_file_buf(struct cgapi_file *f)
 {
 	(void) f;
 	return 0;
 }
+
 // TODO: Send unlink request to server
 int cgapi_unlink_file(struct cgapi_file *f)
 {
