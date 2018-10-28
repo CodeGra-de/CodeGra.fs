9445e8857d4e01574307c400aa19d274acb78850
Thomas Schaper <thomas@libremail.nl>
2018-10-28 02:14:11 +0200

    Add build files

---
+++ build.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+
+if sys.platform.startswith('win32'):
+    os.chdir(os.path.dirname(__file__))
+    subprocess.check_call(
+        [
+            'pyinstaller',
+            os.path.join('codegra_fs', 'gui.py'),
+            '--onefile',
+            '--icon',
+            os.path.join('static', 'windows-icon.ico'),
+            '--noconsole',
+        ]
+    )
+    subprocess.check_call(
+        [
+            'pyinstaller',
+            os.path.join('codegra_fs', 'api_consumer.py'),
+            '--onedir',
+            '--name',
+            'cgapi-consumer',
+        ]
+    )
+    subprocess.check_call(
+        [
+            os.path.join(
+                'C:\\',
+                'Program Files (x86)',
+                'NSIS',
+                'makensis.exe',
+            ),
+            'installer.nsi',
+        ]
+    )
+elif sys.platform.startswith('linux'):
+    subprocess.check_call(['python3', 'sdist', 'bdist_wheel'])
+    print("""You can now upload this dist to pypi using:
+
+    `twine upload dist/*`
+    """)
+else:
+    print('Your platform cannot build cgfs yet')
