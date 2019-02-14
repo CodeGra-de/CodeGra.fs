ed1dd5bd3c2751040ccbac09d11a9df0cb250f40
Olmo Kramer <olmo.kramer@protonmail.com>
2019-02-14 14:10:23 +0100

    Fix latest version links

--- codegra_fs/cgfs.py
+++ codegra_fs/cgfs.py
@@ -2039,7 +2039,7 @@ def check_version() -> None:
                 'You are running an outdated version of'
                 ' CGFS, please consider upgrading.'
             ),
-            'You can do this at https://codegra.de/codegra_fs/latest/',
+            'You can do this at https://codegra.de/codegra_fs/latest',
         ]
         print('-' * (max(len(l) for l in msg) + 4), file=sys.stderr)
         for line in msg:
