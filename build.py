4f7089e587712b4004038393d5bde8addb532b08
CodeGrade <info@CodeGra.de>
2019-03-28 17:03:06 +0100

    Fix Windows installer

--- build.py
+++ build.py
@@ -4,31 +4,47 @@ import os
 import sys
 import subprocess
 
+import requests
+
 if sys.platform.startswith('win32'):
     os.chdir(os.path.dirname(__file__))
-    subprocess.check_call(
-        [
-            'pyinstaller',
-            os.path.join('codegra_fs', 'cgfs.py'),
-            '--onefile',
-            '--icon',
-            os.path.join('static', 'icons', 'ms-icon.ico'),
-        ]
-    )
-    subprocess.check_call(
-        [
-            'pyinstaller',
-            os.path.join('codegra_fs', 'api_consumer.py'),
-            '--onedir',
-            '--name',
-            'cgapi-consumer',
-        ]
-    )
     # subprocess.check_call(
     #     [
-    #         'npm', 'run', 'build:win',
+    #         'pyinstaller',
+    #         os.path.join('codegra_fs', 'cgfs.py'),
+    #         '--onefile',
+    #         '--icon',
+    #         os.path.join('static', 'icons', 'ms-icon.ico'),
     #     ]
     # )
+    # subprocess.check_call(
+    #     [
+    #         'pyinstaller',
+    #         os.path.join('codegra_fs', 'api_consumer.py'),
+    #         '--onedir',
+    #         '--name',
+    #         'cgapi-consumer',
+    #     ]
+    # )
+
+    # url = 'https://github.com/billziss-gh/winfsp/releases/download/v1.4/winfsp-1.4.19016.msi'
+    # print('Downloading winfsp installer from', url)
+    # r = requests.get(url, allow_redirects=True)
+    # open('dist/winfsp.msi', 'wb').write(r.content)
+
+    subprocess.check_call(
+        [
+            os.path.join(
+                'C:\\',
+                'Program Files',
+                'nodejs',
+                'npm',
+            ),
+            'run', 
+            'build:only',
+        ],
+        shell=True,
+    )
 elif sys.platform.startswith('linux'):
     subprocess.check_call(['python3', 'sdist', 'bdist_wheel'])
     print("""You can now upload this dist to pypi using:
