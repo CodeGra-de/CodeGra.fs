9064c34ef9b3fb8eb8bc70e348fc638b4f257ef8
Thomas Schaper <thomas@libremail.nl>
2019-08-19 11:23:06 +0200

    Add script to install fs on Debian/Ubuntu

---
+++ .scripts/install_linux.bash
@@ -0,0 +1,86 @@
+#!/bin/bash
+# SPDX-License-Identifier: AGPL-3.0-only
+VERSION="1.0.0"
+
+err_echo() {
+    (>&2 echo "$@")
+}
+
+is_linux() {
+    [[ "$(uname)" = 'Linux' ]]
+}
+
+get_arch() {
+    dpkg --print-architecture
+}
+
+is_distro() {
+    local to_check="$1"
+    grep "$to_check" /etc/issue >/dev/null 2>&1
+}
+
+install_deps() {
+    sudo apt-get install -qy wget python3 fuse python3-requests libnotify4 gconf2 gconf-service libappindicator1 libxtst6 libnss3
+}
+
+download_file() {
+    local url="$1" dst="$2"
+    if ! wget --quiet "$url" -O "$dst"; then
+        err_echo "Failed to download file"
+        exit 10
+    fi
+}
+
+main() {
+    local tmpdir
+
+    if ! is_linux; then
+        err_echo "This script only supports linux"
+        exit 1
+    fi
+
+    if ! is_distro "Debian" && ! is_distro "Ubuntu"; then
+        err_echo "For now only Ubuntu and Debian are supported"
+        exit 2
+    fi
+
+    case "$(get_arch)" in
+        "amd64") ;;
+        "i386") ;;
+        *) err_echo "We only support amd64 and i386 as architectures"
+           exit 3
+    esac
+
+    echo "Updating package list"
+    sudo apt update -q
+
+    printf "\\nInstalling dependencies\\n"
+    if ! install_deps; then
+        err_echo "Failed to install dependencies"
+        exit 4
+    fi
+    tmpdir="$(mktemp -d)"
+    trap '[[ -n $tmpdir ]] && rm -rf "$tmpdir"' 0 1 2 3 15
+
+    printf "\\nDownloading all needed files\\n"
+    download_file "https://codegra.de/static/fs/linux/python3-fusepy_NEWEST-1_all.deb" "$tmpdir/fusepy.deb"
+    if is_distro "Debian"; then
+        download_file "https://codegra.de/static/fs/debian/python3-codegrade-fs_${VERSION}-1_all.deb" "$tmpdir/backend.deb"
+    else
+        download_file "https://codegra.de/static/fs/ubuntu/python3-codegrade-fs_${VERSION}-1_all.deb" "$tmpdir/backend.deb"
+    fi
+    download_file "https://codegra.de/static/fs/linux/codegrade-fs_${VERSION}_$(get_arch).deb" "$tmpdir/frontend.deb"
+
+    printf "\\nInstalling our version of fusepy\\n"
+    sudo dpkg -i "$tmpdir/fusepy.deb"
+    printf "\\nInstalling the backend of the CodeGrade Filesystem\\n"
+    sudo dpkg -i "$tmpdir/backend.deb"
+    printf "\\nInstalling the frontend of the CodeGrade Filesystem\\n"
+    sudo dpkg -i "$tmpdir/frontend.deb"
+    rm -rf "$tmpdir"
+    tmpdir=""
+
+    printf "\\nDone installing the file system\\n"
+}
+
+main
