50d99b53fe6043382ed514377394f73d6335191f
Thomas Schaper <libre-man@users.noreply.github.com>
2020-03-09 11:57:37 +0100

    Update version to 1.1.2 (#53)

--- codegra_fs/__init__.py
+++ codegra_fs/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
-__version__ = (1, 1, 1)
+__version__ = (1, 1, 2)
 __author__ = 'Olmo Kramer, Thomas Schaper'
 __email__ = 'info@CodeGra.de'
 __license__ = 'AGPL-3.0-only'
